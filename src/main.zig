8b6559e82f35dd2f9dea5933148fe94893734e0c
Mathieu Dumont <dumont.m@icloud.com>
2024-12-26 09:00:46 -0500

    Execute the boot sequence and output some temporary debug logs

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,30 @@ const std = @import("std");
 const CPU = @import("cpu.zig").CPU;
 const MemoryBus = @import("memory_bus.zig").MemoryBus;
 const addr = @import("addressable_space.zig");
+const OpCode = @import("opcodes.zig").OpCode;
 const boot_rom: *const [addr.BOOT_ROM_SIZE:0]u8 = @embedFile("dmg_boot.bin");
 
 pub fn main() !void {
     var cpu = CPU{ .memoryBus = MemoryBus.init(boot_rom) };
 
+    while (cpu.registers.program_counter < 256) {
+        var intruction_byte = cpu.memoryBus.readByte(cpu.registers.program_counter);
+        const is_prefixed = intruction_byte == 0xCB;
+        if (is_prefixed) {
+            intruction_byte = cpu.memoryBus.readByte(cpu.registers.program_counter + 1);
+        }
 
+        const instruction = if (is_prefixed) OpCode{ .prefixed = @enumFromInt(intruction_byte) } else OpCode{ .unprefixed = @enumFromInt(intruction_byte) };
 
-    _ = cpu.step();
+        _ = cpu.step();
 
-    std.debug.print("{b}\n", .{cpu.registers.getAF()});
+        switch (instruction) {
+            .unprefixed => |*unprefixed| {
+                std.debug.print("[unprefixed] {s}\n", .{@tagName(unprefixed.*)});
+            },
+            .prefixed => |*prefixed| {
+                std.debug.print("[prefixed]   {s}\n", .{@tagName(prefixed.*)});
+            },
+        }
+    }
 }
